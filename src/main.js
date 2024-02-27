03e05bf7b95d4d18ae3d306cb1c71183db0df929
olidec <69003898+olidec@users.noreply.github.com>
2024-02-27 12:18:58 +0100

    starting whackamole

--- src/main.js
+++ src/main.js
@@ -2,116 +2,51 @@ import kaboom from "kaboom";
 
 // initialize context
 kaboom();
-setGravity(1200);
 
 // load assets
-loadSpriteAtlas("sprites/player_tilesheet.png",
+loadSpriteAtlas("sprites/sprites.png",
 {
-  "player": {
+  "hole": {
     "x": 0,
     "y": 0,
-    "width": 720,
-    "height": 330,
-    "sliceX": 9,
-    "sliceY": 3,
+    "width": 1140,
+    "height": 1152,
+    "sliceX": 6,
+    "sliceY": 8,
     anims: {
-      idle: 23,
-      run: { from: 9, to: 10, speed: 4, loop: true, },
+      empty: 0,
+      popup: { from: 1, to: 5, speed: 4, loop: false, },
     },
   },
 });
-// loadSprite("bean", "sprites/bean.png");
 
 scene("game", () => {
 
-const bean = add([
-  sprite("player", {anim: "idle"}),
-  pos(800, 400),
-  scale(1),
-  area(),
-  body()
-]);
-
-// add ground
-add([
-  rect(width(), 48),
-  pos(0, height() - 48),
-  outline(4),
-  area(),
-  body({ isStatic: true }),
-  color(127, 200, 255),
-])
-
-// add tree
-function spawnTree() {
-  add([
-    rect(48, 64),
-    area(),
-    outline(4),
-    pos(width(), height() - 48),
-    anchor("botleft"),
-    color(255, 180, 255),
-    move(LEFT, 240),
-    "tree"
-  ]);
-  wait(1.5, () => {
-    spawnTree();
-  })
+for (let n = 0; n < 5; n++) {
+  for (let i = 0; i < 3; i++) {
+    Rad
+    add([ sprite("hole"), 
+    anchor("center"),
+    "hole", 
+    pos(150+width()/7.5*n, 100+height()/4.5*i), 
+    {anim: "popup"} ]);
+  }
 }
 
-// spawnTree();
 
-bean.onCollide("tree", () => {
-  addKaboom(bean.pos);
-  shake();
-  go("game-over");
-});
-
-onKeyPress("space", () => {
-  if (bean.isGrounded()) {
-    bean.jump();
-  }
-})
-
-onKeyDown("right", () => {
-  bean.move(240, 0);
-})
-
-onKeyPress("right", () => {
-  bean.flipX = false;
-  bean.play("run");
-})
-
-onKeyRelease("right", () => {
-  bean.play("idle");
-})
-
-onKeyDown("left", () => {
-  bean.move(-240, 0);
-})
-
-onKeyPress("left", () => {
-  bean.flipX = true;
-  bean.play("run");
-})
-
-onKeyRelease("left", () => {
-  bean.play("idle");
-})
-
-let score = 0;
-const scoreLabel = add([
-    text(score),
-    pos(24, 24)
-])
+// let score = 0;
+// const scoreLabel = add([
+//     text(score),
+//     pos(24, 24)
+// ])
 
 // increment score every frame
 onUpdate(() => {
-  score++;
-  scoreLabel.text = score;
-  if(bean.pos.y >= height()) {
-    go("game-over");
-  }
+  // score++;
+  // scoreLabel.text = score;
+  // if(bean.pos.y >= height()) {
+  //   go("game-over");
+  // }
 });
 
 });
